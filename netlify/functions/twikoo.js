942bfe817847e4074f6ad484a6f1ad84ac47aece
少不钒 <1712686898@qq.com>
2026-03-29 21:05:22 +0800

    Update twikoo.js

--- netlify/functions/twikoo.js
+++ netlify/functions/twikoo.js
@@ -1,1 +1,30 @@
-exports.handler = require('twikoo-netlify').handler
+const twikoo = require('twikoo-netlify');
+
+exports.handler = async (event, context) => {
+
+  // ===== 处理 OPTIONS（预检请求）=====
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "https://shaobufan.github.io",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
+      },
+      body: ""
+    };
+  }
+
+  // ===== 正常请求 =====
+  const res = await twikoo.handler(event, context);
+
+  return {
+    ...res,
+    headers: {
+      ...(res.headers || {}),
+      "Access-Control-Allow-Origin": "https://shaobufan.github.io",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "POST, OPTIONS"
+    }
+  };
+};
